7bce3bd85d2f3c019b9f0066b56f122693c3f34c
Tanuka Das <das.tanuka@yahoo.com>
2019-03-29 01:38:43 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -24,6 +24,7 @@ function doWhileLoop(num) {             //integer
   var i = 0
   function incrementVariable(){
     i = i + 1;
+    return i;
   }
   do {
     console.log("I run once regardless.")
